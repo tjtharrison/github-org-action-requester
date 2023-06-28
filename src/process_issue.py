fff51018335864d67a2c576a7ea0a74f26eb2fed
tjtharrison <gh@tjth.co>
2023-06-28 21:42:15 +0100

    fix: Fixed linting

--- src/process_issue.py
+++ src/process_issue.py
@@ -2,7 +2,6 @@
 
 import configparser
 import os
-import shutil
 import subprocess
 
 # If not on github actions, load .env file
@@ -15,7 +14,7 @@ gh_issue_title = os.environ["GH_ISSUE_TITLE"]
 gh_issue_body = os.environ["GH_ISSUE_BODY"]
 gh_issue_number = os.environ["GH_ISSUE_NUMBER"]
 gh_issue_author = os.environ["GH_ISSUE_AUTHOR"]
-action_dir = "action_dir"
+ACTION_DIR = "action_dir"
 
 
 def read_issue_body():
@@ -73,24 +72,22 @@ def validate_inputs(action_requests):
     """
     print("Creating working directory")
     try:
-        os.mkdir(action_dir)
+        os.mkdir(ACTION_DIR)
     except FileExistsError:
         pass
 
     print("Validating action request inputs")
     for action_request in action_requests:
         action_name = action_requests[action_request]["name"]
-        repo_name = action_name.split("/")[1]
-        action_description = action_requests[action_request]["description"]
         action_version = action_requests[action_request]["version"]
 
         print(f"Processing {action_name}@{action_version}")
 
         # Check if action is available
-        print(f"Checking if action is available")
+        print("Checking if action is available")
         try:
             subprocess.check_output(
-                f"gh repo clone {action_name} {action_dir} -- -b {action_version}",
+                f"gh repo clone {action_name} {ACTION_DIR} -- -b {action_version}",
                 shell=True,
                 stderr=subprocess.STDOUT,
             )
@@ -123,24 +120,18 @@ def main():
         print(f"Missing field on requested action: {error_message}")
         return False
     except configparser.MissingSectionHeaderError as error_message:
-        print(f"No actions requested in issue body")
+        print("No actions requested in issue body")
         return False
 
     # Validate inputs
     try:
         validate_inputs(action_requests)
-    except Exception as e:
-        print(f"Error processing issue: {e}")
+    except Exception as error_message:
+        print(f"Error processing issue: {error_message}")
         return False
 
     print()
     print("Issue processed successfully")
-    print("Cleaning up working directory")
-    try:
-        shutil.rmtree(action_dir, ignore_errors=True)
-    except OSError as error_message:
-        print(f"Error cleaning up working directory: {error_message}")
-        return False
 
     print("All done, exiting")
     return True
